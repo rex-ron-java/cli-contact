eb05d2217c99fec48c7ad795b28f6855aa72abeb
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-18 09:22:52 -0500

    Created Contact object and constructor.

---
+++ src/Contact.java
@@ -0,0 +1,11 @@
+public class Contact {
+    String firstName;
+    String lastName;
+    long phoneNumber;
+
+    public Contact(String firstName, String lastName, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
